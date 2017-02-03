01de1b9c52f611f62e6b213c823b32de231d98e9
Jared Pearson <jaredapearson@gmail.com>
2017-02-03 06:13:16 -0800

    Fix issue with auth.js

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,17 +1,14 @@
 'use strict';
 
-// middleware for authentication
-module.exports = {
-    /**
-     * Middleware that checks to see if the request is associated to 
-     * a logged in user. If it isn't, then a redirect is sent back to
-     * the login page.
-     */
-    auth(req, res, next) {
-        if (!req.session.user_id) {
-            res.redirect('/login');
-        } else {
-            next();
-        }
+/**
+ * Middleware that checks to see if the request is associated to 
+ * a logged in user. If it isn't, then a redirect is sent back to
+ * the login page.
+ */
+module.exports = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
     }
 };
